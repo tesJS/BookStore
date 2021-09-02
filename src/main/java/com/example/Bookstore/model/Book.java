9f7ddc253c09dae3e5da2fc38f07d51dd69bc638
tesJS <67068244+tesJS@users.noreply.github.com>
2021-09-02 16:18:15 +0300

    Add files via upload
    Added the project from eclipse via drag and drop

---
+++ src/main/java/com/example/Bookstore/model/Book.java
@@ -0,0 +1,11 @@
+package com.example.Bookstore.model;
+
+public class Book {
+	
+	 private String title;
+	 private String author;
+	 private String year;
+	 private String isbn;
+	 private Long price;
+
+}
